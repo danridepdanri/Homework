abebd4156f811f7d94ffc5c1d85529c76689f2ef
Danridep <ostapets.yarika@gmail.com>
2022-06-23 12:58:36 +0300

    Сделал класс Human

---
+++ Human/Human.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human
+{
+    class Human
+    {
+        private string _name;
+        private int _age;
+        private int _wight;
+        private string _sex;
+
+        public Human(string name, int age)
+        {
+            _name = name;
+            _age = age;
+            
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public void SetAge(int age)
+        {
+            _age = age;
+        }
+
+       
+
+    }
+
+    
+}
